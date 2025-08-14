e9cb89a61dda948f027fea9556eaeef4fbe48f98
Mahmedk77 <mak.212601@gmail.com>
2025-08-14 20:14:39 +0500

    fix deploy

--- front-end/src/context/ShopContext.jsx
+++ front-end/src/context/ShopContext.jsx
@@ -9,7 +9,7 @@ const ShopContextProvider = (props) => {
 
     const currency = "$";
     const delivery_fee = 10;
-    const backendUrl = "https://mern-e-commerce-production-abc2.up.railway.app";
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
     const [showSearch, setShowSearch] = useState(false);
     const [search, setSearch] = useState("");
     const [cartItems, setCartItems] = useState({});
